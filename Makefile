80e08a5d8758e4bc48dffc08027fc492ce2df0e8
Maxime Le Conte des Floris <git@mlcdf.fr>
2016-12-17 12:47:50 +0100

    Add rust

--- Makefile
+++ Makefile
@@ -42,6 +42,11 @@ ruby:
 	gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 	curl -sSL https://get.rvm.io | bash -s stable --ruby=2.3.1 --rails --ignore-dotfiles
 
+rust:
+	curl -sSf https://static.rust-lang.org/rustup.sh | sh
+	cargo install racer
+
+
 zsh:
 	sudo apt-get install zsh --yes
 	chsh -s "$(which zsh)"
--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -4,7 +4,10 @@ export EDITOR='subl'
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-export PATH="/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
 export MANPATH="/usr/local/man:$MANPATH"
 
+export PATH="/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
 export PATH="$PATH:$HOME/.rvm/bin"
+export PATH="$PATH:$HOME/.cargo/bin"
+
+export RUST_SRC_PATH="$HOME/.multirust/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src"
