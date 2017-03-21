805ca3a2d0f268a81a8ea1eb8d88c465075f9800
Maxime Le Conte des Floris <git@mlcdf.fr>
2017-03-21 17:01:37 +0100

    Fix plugin name

--- .hyper.js
+++ .hyper.js
@@ -82,7 +82,7 @@ module.exports = {
   //   `@company/project`
   //   `project#1.0.1`
   plugins: [
-    // 'hyper-punk'
+    //'hyperpunk',
     //'hyper-snazzy',
     'hyperterm-firewatch',
     'hyper-blink',
