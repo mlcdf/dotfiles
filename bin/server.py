33eaa8cda387c7348faf18e1797a83f3a3af31e2
Maxime Le Conte des Floris <git@mlcdf.fr>
2018-02-24 17:11:53 +0100

    bin/server: copy local url to clipboard

--- bin/server.py
+++ bin/server.py
@@ -6,7 +6,12 @@ import webbrowser
 import http.server
 import socket
 import socketserver
+import subprocess
 
+
+def copy_to_clipboard(txt):
+    cmd=f'echo "{txt.strip()}" | xsel --clipboard --input'
+    return subprocess.check_call(cmd, shell=True)
 
 def get_local_ip():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
@@ -18,11 +23,13 @@ def main():
     p.add_option("-p", "--port", dest="port", default=8000,
                   help="specify the port (default: 8000)")
     options, arguments = p.parse_args()
-    Handler = http.server.SimpleHTTPRequestHandler
 
-    with socketserver.TCPServer(("", int(options.port)), Handler) as httpd:
-        print("   Local URL:", f"http://localhost:{options.port}")
-        print("External URL:", f"http://{get_local_ip()}:{options.port}")
+    with socketserver.TCPServer(("", int(options.port)), http.server.SimpleHTTPRequestHandler) as httpd:
+        local_url = f"http://localhost:{options.port}"
+        external_url = f"http://{get_local_ip()}:{options.port}"
+        print("   Local URL:", local_url)
+        print("External URL:", external_url)
+        copy_to_clipboard(local_url)
         httpd.serve_forever()
 
 if __name__ == '__main__':
