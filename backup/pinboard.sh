059784f8ffc1a60c1aadcf2cd5d5723799f97e84
Maxime Le Conte des Floris <git@mlcdf.fr>
2021-03-26 14:23:31 +0100

    fix pinboard backup

--- backup/pinboard.sh
+++ backup/pinboard.sh
@@ -2,9 +2,9 @@
 set -e
 
 if [[ -z "$PINBOARD_TOKEN" ]]; then
-    echo "Must provide GITHUB_TOKEN in environment" 1>&2
+    echo "Must provide PINBOARD_TOKEN in environment" 1>&2
     exit
 fi
 
 mkdir -p pinboard-backup
-curl "https://@api.pinboard.in/v1/posts/all?format=json&auth_token=${PINBOARD_TOKEN}" > pinboard-backup/links.json
+curl "https://api.pinboard.in/v1/posts/all?format=json&auth_token=${PINBOARD_TOKEN}" > pinboard-backup/links.json
