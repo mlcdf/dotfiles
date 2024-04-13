d47b0043b75299df9ca7c922b05973203730ae7d
Maxime Le Conte des Floris <git@mlcdf.fr>
2024-04-13 23:24:58 +0200

    rework backup-saas script

--- files/programs/backup-saas.py
+++ files/programs/backup-saas.py
@@ -2,57 +2,68 @@
 # Performs a local backup of SaaS data
 
 import argparse
-import json
 import os
-import platform
 import subprocess
-import urllib.request
+import shutil
 
-BACKUPS_LOCATION = os.path.expanduser("~/Backups")
-BINARIES_LOCATION = os.path.expanduser("~/.local/bin")
-
-
-def arch() -> str:
-    if platform.machine() == "x86_64":
-        return "amd64"
-
-    if "arm" in platform.machine():
-        if "64" in platform.architecture()[0]:
-            return "arm64"
-        else:
-            return "arm"
-
-    raise Exception("unexpected machine=%s arch=%s" % (platform.machine(), platform.architecture()))
+_BACKUPS_LOCATION = os.path.expanduser("~/Backups")
 
 
-def system() -> str:
-    return platform.system().lower()
+def _setup():
+    systemd_service = """
+    [Unit]
+    Backup data from SAAS softwares.
+
+    [Service]
+    ExecStart=/usr/bin/python3 ~/dotfiles/files/programs/backup-saas.py
+    """
+
+    systemd_timer = """
+    [Unit]
+    Description=Backup SAAS timer.
+
+    [Timer]
+    OnBootSec=2min
+    OnCalendar=hourly
+    AccuracySec=24h
+    Persistent=true
+    Unit=backup-saas.service
+
+    [Install]
+    WantedBy=default.target
+    """
+
+    os.makedirs(os.path.expanduser("~/.config/systemd/user/"), exist_ok=True)
+
+    with open(os.path.expanduser("~/.config/systemd/user/backup-saas.service"), "w") as fd:
+        fd.write(systemd_service)
+    
+    with open(os.path.expanduser("~/.config/systemd/user/backup-saas.timer"), "w") as fd:
+        fd.write(systemd_timer)
+
+    subprocess.run(["systemctl", "--user", "daemon-reload"], check=True)
+
+    subprocess.run(["systemctl", "--user", "start", "backup-saas.timer"], check=True)
+
+    subprocess.run(["systemctl", "--user", "enable", "backup-saas.timer"], check=True)
+
+    subprocess.run(["systemctl", "--user", "list-timers"], check=True)
 
 
-def install():
-    subprocess.run(["python3", "-m", "pip", "install", "github-backup"], shell=True, check=True)
-
-    with urllib.request.urlopen(
-        "https://api.github.com/repos/mlcdf/sc-backup/releases/latest"
-    ) as response:
-        release = json.load(response)
-
-    for asset in release["assets"]:
-        if f"{system()}-{arch()}" in asset["name"]:
-            url = asset["browser_download_url"]
-
-    urllib.request.urlretrieve(url, os.path.join(BINARIES_LOCATION, "sc-backup"))
-
-
-def github():
+def _github():
     if "GITHUB_TOKEN" not in os.environ:
         print("Missing GITHUB_TOKEN in environment")
         exit(1)
 
+    if not shutil.which("github-backup"):
+        subprocess.run(
+            ["python3", "-m", "pip", "install", "github-backup"],
+            check=True,
+        )
+
     subprocess.run(
         [
             "github-backup",
-            "mlcdf",
             "-t",
             os.environ["GITHUB_TOKEN"],
             "--starred",
@@ -60,67 +71,23 @@ def github():
             "--gists",
             "--private",
             "-o",
-            os.path.join(BACKUPS_LOCATION, "GitHub"),
+            os.path.join(_BACKUPS_LOCATION, "GitHub"),
+            "mlcdf",
         ],
-        shell=True,
         check=True,
-    )
-
-
-def senscritique():
-    subprocess.run(["sc-backup", "--collection", "mlcdf"], shell=True, check=True)
-    subprocess.run(
-        [
-            "sc-backup",
-            "--list",
-            "https://www.senscritique.com/liste/Vu_au_cinema/363578",
-            "-o",
-            os.path.join(BACKUPS_LOCATION, "SensCritique"),
-        ],
-        shell=True,
-        check=True,
-    )
-
-
-def pinboard():
-    if "PINBOARD_TOKEN" not in os.environ:
-        print("Missing PINBOARD_TOKEN in environment")
-        exit(1)
-
-    os.makedirs(os.path.join(BACKUPS_LOCATION, "Pinboard"), exist_ok=True)
-    urllib.request.urlretrieve(
-        f"https://api.pinboard.in/v1/posts/all?format=json&auth_token={os.environ['PINBOARD_TOKEN']}",
-        os.path.join(BACKUPS_LOCATION, "Pinboard", "posts.json"),
     )
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--install", action="store_true", help="install prerequisites")
-    parser.add_argument("--github", action="store_true", help="backup GitHub")
-    parser.add_argument("--senscritique", action="store_true", help="backup SensCritique")
-    parser.add_argument("--pinboard", action="store_true", help="backup Pinboard")
-
+    parser.add_argument("--setup", action="store_true")
     args = parser.parse_args()
 
-    if args.install:
-        install()
+    if args.setup:
+        _setup()
         return
 
-    if not args.github and not args.senscritique and not args.pinboard:
-        github()
-        senscritique()
-        pinboard()
-        return
-
-    if args.github:
-        github()
-
-    if args.senscritique:
-        senscritique()
-
-    if args.pinboard:
-        pinboard()
+    _github()
 
 
 if __name__ == "__main__":
